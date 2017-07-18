e982438909da89ff315c8bebb425cc841a7f72db
Emre Pişkin <piskin.emre@gmail.com>
2017-07-18 11:48:07 +0300

    gulpfile.babel.js:
    * Curly braces fix
    * babelify.configure causes errors. So It's replaced by presets as described in docs: https://github.com/babel/babelify#options
    * Gulp default task added for --development mode, otherwise it throws an error
    * Deleted unnecessary imports
    * browserSync.stream() disabled for styles in production mode
    * Minor typos
    
    app.js: "babelify/polyfill" removed as it is unnecessary now
    
    .gitignore: .idea added for IDE folders

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,11 @@
 import gulp from 'gulp';
 import gulploadplugins from 'gulp-load-plugins';
-import runSequence from 'run-sequence';
 import yargs from 'yargs';
 import browserSync from 'browser-sync';
 import browserify from 'browserify';
 import handlebars from 'browserify-handlebars';
-import babelify from 'babelify';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
-import pkgInfo from './package.json';
 
 const $ = gulploadplugins({
     lazy: true
@@ -19,56 +16,57 @@ const argv = yargs.argv;
 // SASS Styles
 gulp.task('styles', () => {
 
-  return gulp.src([
-    'src/vendor/**/*.css',
-    'src/sass/*.scss'
-  ])
-    .pipe($.changed('.tmp/styles', {extension: '.css'}))
-    .pipe($.if(!argv.production,$.sourcemaps.init()))
-    .pipe($.sass({
-      precision: 10
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({ browsers: ['last 10 version'] }))
-    .pipe(gulp.dest('.tmp'))
-    // Concatenate and minify styles if production mode (via gulp styles --production)
-    .pipe($.if('*.css' && argv.production, $.minifyCss()))
-    .pipe($.if(!argv.production,$.sourcemaps.write()))
-    .pipe(gulp.dest('public/css'))
-    .pipe(browserSync.stream())
-    .pipe($.size({title: 'styles'}));
+    return gulp.src([
+        'src/vendor/**/*.css',
+        'src/sass/*.scss'
+    ])
+        .pipe($.changed('.tmp/styles', {extension: '.css'}))
+        .pipe($.if(!argv.production,$.sourcemaps.init()))
+        .pipe($.sass({
+            precision: 10
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({ browsers: ['last 10 version'] }))
+        .pipe(gulp.dest('.tmp'))
+        // Concatenate and minify styles if production mode (via gulp styles --production)
+        .pipe($.if('*.css' && argv.production, $.minifyCss()))
+        .pipe($.if(!argv.production,$.sourcemaps.write()))
+        .pipe(gulp.dest('public/css'))
+        .pipe($.if(!argv.production, browserSync.stream()))
+        .pipe($.size({title: 'styles'}));
 });
 
 // Scripts - app.js is the main entry point, you have to import all required files and modules
-gulp.task("scripts", () => {
+gulp.task('scripts', () => {
 
     return browserify({
-            entries: 'src/js/app.js',
-            debug: true
-        })
-        .transform(babelify.configure({
-            modules: "common"
-        }))
+        entries: 'src/js/app.js',
+        debug: true
+    })
+        .transform("babelify", {presets: ["es2015"]})
         .transform(handlebars)
         .bundle()
         .pipe(source("app.js"))
         .pipe(buffer())
         .pipe($.if(!argv.production,$.sourcemaps.init({loadMaps: true})))
         .pipe($.if(argv.production,$.uglify()))
-          .on('error', $.util.log)
+        .on('error', $.util.log)
         .pipe($.if(!argv.production,$.sourcemaps.write()))
         .pipe(gulp.dest("./public/js"));
 });
 
 // Browser-Sync
 gulp.task('serve', ['styles'], () => {
-  browserSync({
-    notify: false,
-    logPrefix: 'H5ES6',
-    server: ['.tmp', 'public']
-  });
+    browserSync({
+        notify: false,
+        logPrefix: 'H5ES6',
+        server: ['.tmp', 'public']
+    });
 
-  gulp.watch(['public/**/*.html'], browserSync.reload);
-  gulp.watch(['src/sass/**/*.{scss,css}'], ['styles']);
-  gulp.watch(['src/js/**/*.{js,es6,handlebars'], ['scripts', browserSync.reload]);
+    gulp.watch(['public/**/*.html'], browserSync.reload);
+    gulp.watch(['src/sass/**/*.{scss,css}'], ['styles']);
+    gulp.watch(['src/js/**/*.{js,es6,handlebars}'], ['scripts', browserSync.reload]);
 
 });
+
+// Default task
+gulp.task('default', ['styles', 'scripts']);
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
-// include ES6 Browser Polyfill
-import "babelify/polyfill"
-
 // import jQuery
 import "jquery"
 
