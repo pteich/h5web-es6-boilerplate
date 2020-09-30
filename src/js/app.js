40c0b302b80182bc4fc80fabede7363909d5f089
Tarun Gehlot <56501441+tarun1802@users.noreply.github.com>
2020-09-30 18:27:43 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@
 import HelloTemplate from "../templates/hello.handlebars"
 
 // Example 2: Compile the template
-let hellohtml = HelloTemplate({ name: 'Peter' });
+let hellohtml = HelloTemplate({ Name: 'Peter' });
 
 // Example 2: Render the template
 document.getElementById('container').innerHTML = hellohtml;
